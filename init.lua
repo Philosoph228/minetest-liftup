3e5ce2287d6ae8acd7c1dbf72476aad8c5617956
Philosoph228 <philosoph228@gmail.com>
2017-03-27 15:04:56 +0500

    Update version 0.1.0

--- init.lua
+++ init.lua
@@ -27,51 +27,69 @@ minetest.register_node("liftup:liftup_block", {
 		
 		local x = pos.x
 		local y = math.max(-30927, pos.y)
-		local orig_y = pos.y
 		local z = pos.z
 		
 		local free = 0
-		local free_above = false
+		local concrete = 0
+		local limit = 100
 		
-		while y <= 30926 do
+		-- Search for non-air block
+		while free <= limit do
 			y = y+1
+			
 			local upper_node = minetest.get_node({x=x, y=y, z=z})
 			
 			if upper_node.name == "ignore" then
 				minetest.get_voxel_manip():read_from_map({x=x, y=y, z=z}, {x=x, y=y, z=z})
-				upper_node = minetest.get_node(pos)
+				upper_node = minetest.get_node({x=x, y=y, z=z})
 			end
 			
-			if upper_node.name == "air" then
-				if y == orig_y+2 then
-					free_above = true;
-				end
-				
-				if free_above == false then
-					free = free+1;
-				end
-			else
+			if upper_node.name ~= "air" then
 				free = 0
-				free_above = false
+				break
+			else
+				free = free+1
+			end
+		end
+		
+		if free > limit then
+			return
+		end	
+		
+		while y < 30926 do
+			y = y+1
+			
+			local upper_node = minetest.get_node({x=x, y=y, z=z})
+			
+			if upper_node.name == "ignore" then
+				minetest.get_voxel_manip():read_from_map({x=x, y=y, z=z}, {x=x, y=y, z=z})
+				upper_node = minetest.get_node({x=x, y=y, z=z})
 			end
 			
-			if free == 2 then
-				if free_above == false then
-					if y-2 ~= orig_y then
+			if upper_node.name ~= "air" then
+				free = 0
+				concrete = concrete+1
+			else
+				free = free+1
+				if free == 2 then
+					local stand_on = minetest.get_node({x=x, y=y-2, z=z})					
+					if minetest.registered_nodes[stand_on.name].damage_per_second ~= 0 then
+						minetest.chat_send_player(clicker:get_player_name(), "It's unsafe to lift up here because "..stand_on.name..".")
+					else
 						clicker:setpos({x=x, y=y-1.5, z=z})
 					end
+					return
 				end
-				return
 			end
-		end	
+		end
 	end,
 })
 
 minetest.register_craft({
 	output = 'liftup:liftup_block',
 	recipe = {
-		{'default:diamond', 'default:diamond', 'default:diamond'},
-		{'default:diamond', 'group:stick', 'default:diamond'},
-		{'default:diamond', 'default:diamond', 'default:diamond'},
+		{'default:cobble', 'default:cobble', 'default:cobble'},
+		{'default:gold_ingot', 'group:diamond', 'default:gold_ingot'},
+		{'default:cobble', 'default:cobble', 'default:cobble'},
 	}
 })
